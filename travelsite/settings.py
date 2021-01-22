ecf671e38181b1af29e4af62c560111067b19b5f
myfrank4everreal <myfrank4everreal@gmail.com>
2021-01-22 22:33:43 +0400

    cleanup

--- travelsite/settings.py
+++ travelsite/settings.py
@@ -183,7 +183,7 @@ AWS_DEFAULT_ACL = "None"
 DEFAULT_FILE_STOREAGE= "storages.backends.s3boto3.S3Boto3Storage"
 
 
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 
