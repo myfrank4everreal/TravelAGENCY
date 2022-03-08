011cec91948f50a8bf59341860865536faf206dc
myfrank4everreal <myfrank4everreal@gmail.com>
2022-03-08 21:58:47 +0400

    None

--- travelsite/settings.py
+++ travelsite/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['https://travel-agent-visa.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['http://presictravels.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
